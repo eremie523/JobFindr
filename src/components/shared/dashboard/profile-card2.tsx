c10817b38ae81012d2735b747b43b6e4fb34a098
Johnson Eremie <122898606+eremie523@users.noreply.github.com>
2024-08-05 13:06:28 +0100

    Update profile-card2.tsx

--- src/components/shared/dashboard/profile-card2.tsx
+++ src/components/shared/dashboard/profile-card2.tsx
@@ -15,7 +15,7 @@ const ProfileCard2 = ({name, email, bio, phoneNo, matricNo}: Props) => {
   return (
     <Card className="lg:w-1/3 w-full flex flex-col gap-3 p-5 bg-white relative items-center">
         <div className='flex flex-col items-center text-center'>
-          <Image src={"/vercel.svg"} alt='profile' width={150} height={150} className={'object-cover border rounded-full h-[150px]'}></Image>
+          <Image src={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2-YaGxKSXWxV6rQcK9JP0APjqK7ujmcjqCQ&usqp=CAU"} alt='profile' width={150} height={150} className={'object-cover border rounded-full h-[150px]'}></Image>
           <div>
             <h3 className='text-2xl'>{name}</h3>
             <p className='text-small text-gray-600'>{bio}</p>
