19cdfba268fdb4335bbcceaeadcba55a25a64a7b
Johnson Eremie <reremie523@gmail.com>
2024-08-05 21:11:36 +0100

    Update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
+import { Playfair_Display } from 'next/font/google';
 import Image from 'next/image'
 
-export default function Home() {
+const headerFont = Playfair_Display({ subsets: ["latin"] });
+
+export default function Page() {
   return (
-    <main>
-      
+    <main className='h-screen w-screen relative flex items-center justify-center bg-accent-color-1'>
+      <div className='flex flex-col'>
+        <div className={'text-xxlarge text-accent-color-1 font-bold max-md:text-center'}>
+          <h3 className={`${headerFont.className}`}>JobFindr</h3>
+        </div>
+      </div>
+      {/* <Image src={'/assets/images/landing.jpg'} alt='Landing Bg' width={100} height={100} className='w-screen h-screen object-cover absolute top-0 left-0'></Image> */}
     </main>
   )
 }
