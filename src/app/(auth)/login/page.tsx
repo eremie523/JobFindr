43a4bce5c6167e08546fa6dece667b327b946e8c
Johnson Eremie <122898606+eremie523@users.noreply.github.com>
2024-08-05 19:33:01 +0100

    Update page.tsx

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -8,7 +8,7 @@ import useLogin from '@/hooks/useLogin'
 const page = (props: any) => { 
   const { onHandleSubmit, register, reset, errors, loading } = useLogin();
   return (
-    <div className='flex-grow flex w-full flex-col items-center justify-center pb-8 lg:px-4'>
+    <div className='flex-grow flex w-full flex-col items-center justify-center lg:pb-8 lg:px-4'>
         <Card className='border-2 border-accent-color-1 max-lg:shadow-lg max-w-[600px] lg:min-w-[500px] min-w-full flex flex-col py-22 min-h-[450px] justify-center'>
           <Loader1 loading={loading}>
             <LoginForm onHandleSubmit={onHandleSubmit} register={register} errors={errors} />
