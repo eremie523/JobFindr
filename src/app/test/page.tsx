f094df06b5a81c6c3bcf44faa5e4f765329a15e0
Johnson Eremie <reremie523@mail.com>
2024-08-24 16:58:07 +0100

    Edge store update - test - Johnson

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -3,7 +3,7 @@ import { FileState, MultiFileDropzone } from '@/components/shared/multi-file'
 import { SingleImageDropzone } from '@/components/shared/single-upload'
 import { Button } from '@/components/ui/button'
 import { Progress } from '@/components/ui/progress'
-import { useEdgeStore } from '@/lib/edgestore'
+import { EdgeStoreProvider, useEdgeStore } from '@/lib/edgestore'
 import React, { useState } from 'react'
 
 type Props = {}
@@ -30,79 +30,81 @@ const page = (props: Props) => {
   }
 
   return (
-    <div className='flex flex-col items-center justify-center p-5 gap-5'>
-      <SingleImageDropzone
-        width={200}
-        height={200}
-        value={file}
-        dropzoneOptions={{
-          maxSize: 1024 * 1024 * 5,
-          maxFiles: 1,
-        }}
-        onChange={(file) => {
-          setFile(file);
-        }}
-      />
-      <Progress value={progress} />
-      <Button variant={'outline'} onClick={async () => {
-        if (file) {
-          const res = await edgestore.myPublicImages.upload({
-            file,
-            input: { type: "post" },
-            onProgressChange: (progress) => {
-              setProgress(progress);
-            }
-          });
-
-          setImgUrl(res.url);
-          setThumbnailUrl(res.thumbnailUrl);
-          //Save data to DB;
-        }
-      }}> Upload File </Button>
-
-      <div>
-        {imgUrl} <br />
-        {thumnailUrl}
-      </div>
-
-      <div>
-        <MultiFileDropzone
-          value={fileStates}
-          onChange={(files) => {
-            setFileStates(files);
+    <EdgeStoreProvider>
+      <div className='flex flex-col items-center justify-center p-5 gap-5'>
+        <SingleImageDropzone
+          width={200}
+          height={200}
+          value={file}
+          dropzoneOptions={{
+            maxSize: 1024 * 1024 * 5,
+            maxFiles: 1,
           }}
-          onFilesAdded={async (addedFiles) => {
-            setFileStates([...fileStates, ...addedFiles]);
-            await Promise.all(
-              addedFiles.map(async (addedFileState) => {
-                try {
-                  const res = await edgestore.myProtectedFiles.upload({
-                    options: {
-                      temporary: true //This uploads the file temporary for 24 hrs until the upload is confirmed to Confirm simply run 
-                    },
-                    file: addedFileState.file,
-                    input: { type: "protected" },
-                    onProgressChange: async (progress: number) => {
-                      updateFileProgress(addedFileState.key, progress);
-                      if (progress === 100) {
-                        // wait 1 second to set it to complete
-                        // so that the user can see the progress bar at 100%
-                        await new Promise((resolve) => setTimeout(resolve, 1000));
-                        updateFileProgress(addedFileState.key, 'COMPLETE');
-                      }
-                    },
-                  });
-                  console.log(res);
-                } catch (err) {
-                  updateFileProgress(addedFileState.key, 'ERROR');
-                }
-              }),
-            );
+          onChange={(file) => {
+            setFile(file);
           }}
         />
-        {/* {edgestore.myProtectedFiles.confirmUpload({url})} Run this to confirm upload of temporary files that is to retain the file, ex if the file is to be confirmed when the person has successfully signed up then do that */}
+        <Progress value={progress} />
+        <Button variant={'outline'} onClick={async () => {
+          if (file) {
+            const res = await edgestore.myPublicImages.upload({
+              file,
+              input: { type: "post" },
+              onProgressChange: (progress) => {
+                setProgress(progress);
+              }
+            });
+
+            setImgUrl(res.url);
+            setThumbnailUrl(res.thumbnailUrl);
+            //Save data to DB;
+          }
+        }}> Upload File </Button>
+
+        <div>
+          {imgUrl} <br />
+          {thumnailUrl}
+        </div>
+
+        <div>
+          <MultiFileDropzone
+            value={fileStates}
+            onChange={(files) => {
+              setFileStates(files);
+            }}
+            onFilesAdded={async (addedFiles) => {
+              setFileStates([...fileStates, ...addedFiles]);
+              await Promise.all(
+                addedFiles.map(async (addedFileState) => {
+                  try {
+                    const res = await edgestore.myProtectedFiles.upload({
+                      options: {
+                        temporary: true //This uploads the file temporary for 24 hrs until the upload is confirmed to Confirm simply run 
+                      },
+                      file: addedFileState.file,
+                      input: { type: "protected" },
+                      onProgressChange: async (progress: number) => {
+                        updateFileProgress(addedFileState.key, progress);
+                        if (progress === 100) {
+                          // wait 1 second to set it to complete
+                          // so that the user can see the progress bar at 100%
+                          await new Promise((resolve) => setTimeout(resolve, 1000));
+                          updateFileProgress(addedFileState.key, 'COMPLETE');
+                        }
+                      },
+                    });
+                    console.log(res);
+                  } catch (err) {
+                    updateFileProgress(addedFileState.key, 'ERROR');
+                  }
+                }),
+              );
+            }}
+          />
+          {/* {edgestore.myProtectedFiles.confirmUpload({url})} Run this to confirm upload of temporary files that is to retain the file, ex if the file is to be confirmed when the person has successfully signed up then do that */}
+        </div>
       </div>
-    </div>
+    </EdgeStoreProvider>
   )
 }
 
